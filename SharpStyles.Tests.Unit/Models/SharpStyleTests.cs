fae2bbea66adfc5d7cfafa973b2427700cd2129b
Mabrouk Mahdhi <coding@mahdhi.com>
2025-03-23 17:15:23 +0100

    ShouldSerializeMediaQueryWithCustomCondition -> FAIL

--- SharpStyles.Tests.Unit/Models/SharpStyleTests.cs
+++ SharpStyles.Tests.Unit/Models/SharpStyleTests.cs
@@ -191,6 +191,74 @@ my-element {{
             actualCss.Should().BeEquivalentTo(expectedCss);
         }
 
+        [Fact]
+        public void ShouldSerializeMediaQueryWithCustomCondition()
+        {
+            // given
+            string randomValue = GetRandomString();
+
+            var mobileStyle = new TestStyle
+            {
+                MyElement = new SharpStyle
+                {
+                    BackgroundColor = randomValue
+                },
+
+                MyId = new SharpStyle
+                {
+                    BackgroundColor = randomValue
+                },
+
+                MyClass = new SharpStyle
+                {
+                    BackgroundColor = randomValue
+                },
+
+                MyDeep = new SharpStyle
+                {
+                    BackgroundColor = randomValue
+                }
+            };
+
+            var mediaQuery = new MediaQuery
+            {
+                Only = true,
+                MediaType = "screen",
+                MaxWidth = 768,
+                Styles = mobileStyle,
+                CustomFeature = "orientation: portrait"
+            };
+
+            string expectedCss = @$"
+@media only screen and (max-width: 768px) and (orientation: portrait) {{
+
+
+my-element {{
+	background-color: {randomValue};
+}}
+
+#my-id {{
+	background-color: {randomValue};
+}}
+
+.my-class {{
+	background-color: {randomValue};
+}}
+
+::deep my-deep {{
+	background-color: {randomValue};
+}}
+
+}}
+";
+
+            // when
+            string actualCss = mediaQuery.ToCss();
+
+            // then
+            actualCss.Should().BeEquivalentTo(expectedCss);
+        }
+
 
         internal class TestStyle : SharpStyle
         {
